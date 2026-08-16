b994b5664d2d170537af8aecbe1c2507343b15c6
Ruzzy77 <19502719+Ruzzy77@users.noreply.github.com>
2026-08-16 13:46:48 +0900

    release: rebuild Hypes from master

--- plugins/hypes/src/hypes/_build.py
+++ plugins/hypes/src/hypes/_build.py
@@ -1,7 +1,7 @@
 """Generated Hypes provider-package identity."""
 
 PACKAGE_VERSION = "0.8.0"
-BUILD_ID = "0.8.0+codex.20260816043346"
+BUILD_ID = "0.8.0+codex.20260816044603"
 SOURCE_REPOSITORY = "owners/hypes"
-SOURCE_COMMIT = "38f1996fb65d55cd6d532d76344a55cc9037cba9"
+SOURCE_COMMIT = "01e5f467a6f45fcbe034d03657cc920aa4284b45"
 SOURCE_CLEAN = True
